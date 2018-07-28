905f0a983ac80b4b202ec18dc73c71f1138708fb
allaev <34710484+allaev@users.noreply.github.com>
2018-07-29 02:47:44 +0500

    add more books, add links to wikipedia

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 class BookDetail extends Component {
   render() {
     if (!this.props.book) {
-      return <div>Select a book to get started.</div>
+      return <div>Select a book to get started</div>
     }
 
     return (
@@ -12,8 +12,13 @@ class BookDetail extends Component {
         <h3>Details for:</h3>
         <div>Title: {this.props.book.title}</div>
         <div>Author: {this.props.book.author}</div>
+        <div>Genre: {this.props.book.genre}</div>
         <div>Pages: {this.props.book.pages}</div>
-        <img src={this.props.book.image} class="img-thumbnail"/>
+        <div>
+          <a href={this.props.book.link}>
+            <img src={this.props.book.image} className='img-thumbnail'/>
+          </a>
+        </div>
       </div>
     );
   }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,92 @@
 export default function() {
   return [
-    { title: 'The Hunger Games', author: 'Suzanne Collins', pages: 201, image: 'https://images-na.ssl-images-amazon.com/images/I/41LPBRNaVCL._SX355_BO1,204,203,200_.jpg'},
-    { title: 'Harry Potter and the Sorcerers Stone', author: 'J.K. Rowling', pages: 539, image: 'https://timedotcom.files.wordpress.com/2015/07/harry_potter_and_the_sorcerers_stone.jpg' },
-    { title: 'Fahrenheit 451', author: 'Ray Bradbury', pages: 285, image: 'https://images-na.ssl-images-amazon.com/images/I/41qI9quGIdL._SX324_BO1,204,203,200_.jpg' },
-    { title: 'The Lord of the Rings', author: 'J. R. R. Tolkien', pages: 450, image: 'https://images-na.ssl-images-amazon.com/images/I/51tW-UJVfHL._SX321_BO1,204,203,200_.jpg' }
+    {
+      title: 'Crime and Punishment ',
+      author: 'Fyodor Dostoevsky',
+      genre: '  Philosophical novel, Psychological fiction',
+      pages: 574,
+      link: 'https://en.wikipedia.org/wiki/Crime_and_Punishment',
+      image: 'https://upload.wikimedia.org/wikipedia/en/4/4b/Crimeandpunishmentcover.png'
+    },
+    {
+      title: 'Fahrenheit 451',
+      author: 'Ray Bradbury',
+      genre: 'Dystopian',
+      pages: 158,
+      link: 'https://en.wikipedia.org/wiki/Fahrenheit_451',
+      image: 'https://upload.wikimedia.org/wikipedia/en/d/db/Fahrenheit_451_1st_ed_cover.jpg'
+    },
+    {
+      title: 'Gone with the Wind',
+      author: 'Margaret Mitchell',
+      genre: 'Historical Fiction',
+      pages: 1024,
+      link: 'https://en.wikipedia.org/wiki/Gone_with_the_Wind_(novel)',
+      image: 'https://upload.wikimedia.org/wikipedia/en/6/6b/Gone_with_the_Wind_cover.jpg'
+    },
+    {
+      title: 'Harry Potter and the Philosopher\'\s Stone',
+      author: 'J.K. Rowling',
+      genre: 'Fantasy',
+      pages: 336,
+      link: 'https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone',
+      image: 'https://upload.wikimedia.org/wikipedia/en/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg'
+    },
+    {
+      title: 'Memoirs of a Geisha',
+      author: 'Arthur Golden',
+      genre: 'Historical novel',
+      pages: 448,
+      link: 'https://en.wikipedia.org/wiki/Memoirs_of_a_Geisha',
+      image: 'https://upload.wikimedia.org/wikipedia/en/d/dc/MemoirsOfAGeisha.jpg'
+    },
+    {
+      title: 'The Da Vinci Code',
+      author: 'Dan Brown',
+      genre: 'Mystery, Detective fiction, Conspiracy fiction, Thriller',
+      pages: 454,
+      link: 'https://en.wikipedia.org/wiki/The_Da_Vinci_Code',
+      image: 'https://upload.wikimedia.org/wikipedia/en/6/6b/DaVinciCode.jpg'
+    },
+    {
+      title: 'The Hitchhiker\'\s Guide to the Galaxy',
+      author: 'Douglas Adams',
+      genre: 'Comic science fiction',
+      pages: 250,
+      link: 'https://en.wikipedia.org/wiki/The_Hitchhiker%27s_Guide_to_the_Galaxy_(novel)',
+      image: 'https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg'
+    },
+    {
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      genre: 'Adventure, Dystopian, Science fiction',
+      pages: 374,
+      link: 'https://en.wikipedia.org/wiki/The_Hunger_Games_(novel)',
+      image: 'https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg'
+    },
+    {
+      title: 'The Lord of the Rings',
+      author: 'J. R. R. Tolkien',
+      genre: 'Fantasy, Adventure',
+      pages: 450,
+      link: 'https://en.wikipedia.org/wiki/The_Lord_of_the_Rings',
+      image: 'https://upload.wikimedia.org/wikipedia/en/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif'
+    },
+    {
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      genre: 'Southern Gothic, Bildungsroman',
+      pages: 281,
+      link: 'https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird',
+      image: 'https://upload.wikimedia.org/wikipedia/en/7/79/To_Kill_a_Mockingbird.JPG'
+    },
+    {
+      title: 'Twilight',
+      author: 'Stephenie Meyer',
+      genre: 'Fantasy, Romance',
+      pages: 544,
+      link: 'https://en.wikipedia.org/wiki/Twilight_(Meyer_novel)',
+      image: 'https://upload.wikimedia.org/wikipedia/en/1/1d/Twilightbook.jpg'
+    }
   ];
 }
